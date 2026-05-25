2ca4fd6d7ef13cc349a031791ef538fc3ba39dbc
carlosbaldeoncaro5-web18 <carlosbaldeoncaro5@gmail.com>
2026-05-25 18:58:05 -0500

    Problema1

--- Problema1.py
+++ Problema1.py
@@ -1,10 +1,10 @@
 
-
 """
 Cree un proyecto en Python donde cree un arreglo de int de un tamaño indicado por el usuario y se llene con números aleatorios de entre 2 a 4 cifras. Cree una función recursiva que indique el promedio entre el máximo y mínimo valor del arreglo tal que sean múltiplos de 3.
 Por ejemplo si se tiene [5, 6, 10, 9, 12, 14] -> resultado = (12+6)/2 = 9
 
 """
+import random
 
 def promedioMult3(lst):
     ult = len(lst) - 1
@@ -17,17 +17,15 @@ def promMult3(lst, pos, mayor, menor):
         if lst[pos] % 3 == 0:
             mayor = lst[pos] if mayor == None or lst[pos] > mayor else mayor
             menor = lst[pos] if menor == None or lst[pos] < menor else menor
-
         return promMult3(lst, pos - 1, mayor, menor)
 
-tam = int(input("Ingrese tamaño del arreglo: "))
+tam = int(input("Por favor, ingrese tamaño del arreglo: "))
 
 lista = []
 
 for i in range(tam):
-    num = int(input("Por favor, ingrese un numero de 2 a 4 cifras: "))
-    lista.append(num)
+    lista.append(random.randint(10, 9999))
 
-print(f"la lista ingresada fue: {lista}")
+print(f"la lista generada fue: {lista}")
 
 print(f"el promedio es: {promedioMult3(lista)}")
